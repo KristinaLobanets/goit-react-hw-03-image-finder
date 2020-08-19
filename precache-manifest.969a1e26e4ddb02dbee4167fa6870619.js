421a0f15a121d21b3c7c4e4c9f19b44bcf945ac1
Kristina Lobanets <kristina.malitowska@gmail.com>
2020-08-19 14:30:28 +0300

    Updates

---
+++ precache-manifest.969a1e26e4ddb02dbee4167fa6870619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a468f468ef0be63a41cbc3dd1b94f3cc",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "943e5227420d434aa44f",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "6d9ef112ab5cbfdc62e6",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.958fefa3.chunk.css"
+  },
+  {
+    "revision": "943e5227420d434aa44f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.314dde02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.314dde02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9ef112ab5cbfdc62e6",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.00535837.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
